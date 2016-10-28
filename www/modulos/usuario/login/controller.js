8b0d37231c47c77e956a3ed740a13c566b3df03c
Sergio Diniz <sergiodinizsh@gmail.com>
2016-10-28 14:21:17 -0300

    test simple login with angular

--- www/modulos/usuario/login/controller.js
+++ www/modulos/usuario/login/controller.js
@@ -1,3 +1,15 @@
-angular.module('blogjs.usuario').controller('LoginUsuarioController', function($scope, usuarios){
+angular.module('blogjs.usuario').controller('LoginUsuarioController', function($scope, $location, usuarios){
+
+  $scope.usuario = {};
+
+  $scope.entrar = function(usuario){
+    var autenticaUsuario = usuarios.autenticaUsuario(usuario);
+
+    if(autenticaUsuario){
+      $location.path('usuario/' + autenticaUsuario.id + '/posts');
+    }else{
+      alert('Dados invalidos!');
+    }
+  }
 
 })
--- www/modulos/usuario/service.js
+++ www/modulos/usuario/service.js
@@ -1,13 +1,25 @@
 angular.module('blogjs.usuario').factory('usuarios', function(){
 
   var usuarios = [];
+  var usuarioId = 0;
 
   var cadastrar = function(usuario){
+    usuarioId++;
+    usuario.id = usuarioId;
     usuarios.push(usuario);
   }
 
+  var autenticaUsuario = function(usuario){
+    var encontrado = usuarios.find(function(obj){
+      return obj.login === usuario.login && obj.senha === usuario.senha;
+    });
+
+    return encontrado;
+  }
+
   return {
-    cadastrar:cadastrar
+    cadastrar:cadastrar,
+    autenticaUsuario:autenticaUsuario
   }
 
 });
